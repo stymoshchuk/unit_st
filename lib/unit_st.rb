ac873ea77a72666678641590cb6470c995248e52
stymoshchuk <sol.tymoshchuk@gmail.com>
2022-10-11 12:32:31 +0100

    Update docs

--- lib/unit_st.rb
+++ lib/unit_st.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative "unit_st/create_player"
+require_relative "unit_st/update_player"
 require_relative "unit_st/version"
 require "httparty"
 
@@ -9,9 +10,9 @@ module UnitSt
     CreatePlayer.new(first_name, last_name, height_feet, height_inches, position, team_id, weight_pounds).call
   end
 
-  def update_player_info(first_name=nil , last_name=nil, height_feet=nil,
+  def update_player_info(id, first_name=nil , last_name=nil, height_feet=nil,
                          height_inches=nil , position=nil, team_id=nil, weight_pounds=nil)
-    UpdatePlayer.new(id, first_name, last_name).call
+    UpdatePlayer.new(id, first_name, last_name, height_feet, height_inches, position, team_id, weight_pounds).call
   end
 end
 
